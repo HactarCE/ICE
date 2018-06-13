842747dd000abb7bfc42e2b3c5dc619679819dfd
HactarCE <6060305+HactarCE@users.noreply.github.com>
2018-06-13 12:43:37 -0400

    Main menu works

---
+++ Assets/Scripts/TextButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+	public Color IdleColor;
+	public Color HoverColor;
+	public Color ClickColor;
+	//public Color DisabledColor;
+
+	[System.Serializable]
+	public class ClickEvent : UnityEvent { }
+
+	public ClickEvent onClick = new ClickEvent();
+
+	private Text textComponent;
+	private bool hover = false;
+	private bool clicked = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		textComponent = GetComponent<Text>();
+		Rect box = textComponent.rectTransform.rect;
+		BoxCollider boxCollider = (BoxCollider)gameObject.AddComponent(typeof(BoxCollider));
+		boxCollider.size = new Vector3(box.width, box.height, 1);
+		updateColor();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0) && hover)
+		{
+			clicked = true;
+			updateColor();
+		}
+		if (Input.GetMouseButtonUp(0) && clicked)
+		{
+			clicked = false;
+			updateColor();
+			if (hover) onClick.Invoke();
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		hover = true;
+		updateColor();
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		hover = false;
+		updateColor();
+	}
+
+	private void updateColor()
+	{
+		Color color;
+		if (hover)
+		{
+			if (clicked) color = ClickColor;
+			else color = HoverColor;
+			transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+		}
+		else {
+			color = IdleColor;
+			transform.localScale = Vector3.one;
+		}
+		textComponent.color = color;
+		Debug.Log(textComponent);
+		Debug.Log(color);
+	}
+
+}
